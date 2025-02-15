14eef3fc76377b02200816d204163e8542e1148b
rowan <proton.donor.17@gmail.com>
2025-02-14 18:48:40 -0800

    property-based testing (doesn't pass yet) and approximation of W when e^(vmax*t) is out of range

--- kinetics_modeling.py
+++ kinetics_modeling.py
@@ -1,17 +1,27 @@
-import lmfit
 from lmfit import create_params
-from numpy import exp
-from scipy.special import lambertw
+from numpy import log, exp
 
 
 # define objective function: returns the array to be minimized
+from scipy.special import lambertw
+
+
 def objective(params, t, data):
-    s0 = params['[S]_0']
-    k_m = params['K_m']
-    v_max = params['V_max']
-    # TODO: some assertions to catch weird params? to ensure W0 is real-valued?
-    model = k_m * lambertw(s0 / k_m * exp(s0 / k_m - (v_max * t) / k_m))
+    s0 = params['s0']
+    k_m = params['k_m']
+    v_max = params['v_max']
+    # approximation of  k_m * lambertw(s0 / k_m * exp(s0 / k_m - (v_max * t) / k_m))
+    model = k_m * _approx_lambert_w(s0, k_m, v_max, t)
     return model - data
+
+
+def _approx_lambert_w(s0, k_m, v_max, t):
+    log_term = log(s0 / k_m) + (s0 / k_m) - (v_max * t / k_m)
+    if log_term > 10:  # large log_term, use asymptotic formula
+        return log_term - log(log_term) + log(log_term) / log_term
+    if log_term <= -5:  # small log_term, use linear approximation
+        return exp(log_term)
+    return lambertw(exp(log_term)).real
 
 
 def curve_params():
---
+++ test_kinetics_modeling.py
@@ -0,0 +1,24 @@
+import math
+import unittest
+from hypothesis import given, strategies as st
+from lmfit import create_params
+from numpy import clongdouble
+
+from kinetics_modeling import objective
+
+
+class TestKineticsModeling(unittest.TestCase):
+    @given(st.floats(min_value=1e-9, max_value=10),  # s0
+           st.floats(min_value=1e-9, max_value=100),  # k_m
+           st.floats(min_value=1e-9, max_value=100),  # v_max
+           st.integers(min_value=0, max_value=6000))  # t
+    def test_objective_matches_michaelis_menten_kinetics(self, s0, k_m, v_max, t):
+        params = create_params(s0=s0, k_m=k_m, v_max=v_max)
+        model_s = objective(params, t, 0)
+        # $$Vt = ([S]_0-[S]) + K_m \ln\frac{[S]_0}{[S]}$$
+        vt = (s0 - model_s) + k_m * math.log(s0 / model_s)
+        if v_max*t == 0:
+            self.assertAlmostEqual(vt, v_max*t, places=5)
+        else:
+            self.assertAlmostEqual(v_max*t/vt, 1, places=5)
+
