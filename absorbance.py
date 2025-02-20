7411a1efcd3aab6b7cb0ccb823d22f77fa80e033
rowan <proton.donor.17@gmail.com>
2025-02-20 12:38:47 -0800

    remove more dead code

--- absorbance.py
+++ absorbance.py
@@ -3,11 +3,3 @@
 
 path_length = 0.195565054
 extinction = 6220
-
-
-def concentration_from_absorbance(absorbance):
-    return absorbance / (path_length * extinction)
-
-
-def concentration_of_array(absorbance_data):
-    return [[concentration_from_absorbance(x) for x in line] for line in absorbance_data]
