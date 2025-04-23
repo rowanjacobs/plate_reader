40f9bbd518f0fdc503d9771c39c38b5f45ea833d
Rowan Jacobs <rowanjacobs@users.noreply.github.com>
2025-04-23 14:42:12 -0700

    simplify and improve single line processing

--- read_tsv.py
+++ read_tsv.py
@@ -63,33 +63,20 @@ def data_into_replicate_set_timelines(data_lines):
 
 
 def data_into_replicate_set_timelines_single_line(data_lines):
-    wells = data_lines[0].split()[3:]
-    data_line_0 = data_lines[1].split('\t')
-    first_well, last_well = 0, len(wells)
-    started_wells = False
-    for i, string in enumerate(data_line_0[2:]):
-        if string:
-            if not started_wells:
-                first_well = i
-                started_wells = True
-            last_well = i
-        if started_wells and not string:
-            break
+    wells_split = data_lines[0].split('\t')[2:]
+    wells = []
+    for i, string in enumerate(data_lines[1].split('\t')[2:]):
+        if string and string != '\n':
+            wells.append((i+2, wells_split[i]))
 
-    replicate_set_timelines = [ReplicateSetTimeline(well=w, replicate_sets=[]) for w in
-                               wells[first_well:last_well + 1]]
-
-    for line in data_lines[1:]:
-        split = line.split('\t')
-        time = time_in_seconds(split[0])
-        for i in range(first_well + 2, last_well + 3):
-            try:
-                rs = ReplicateSet(time=time, data_points=[float_or_overflow(split[i])],
-                                  well=wells[i - 2])
-                replicate_set_timelines[i - (first_well + 2)].replicate_sets.append(rs)
-            except ValueError:
-                print(f"Value error for well {wells[i - 2]}: could not parse value '{split[i]}' at position {i}")
-                print("Check to see whether the following line was parsed correctly:")
-                exit(ValueError)
-
-    return replicate_set_timelines
+    rstls = []
+    for i, well in wells:
+        rstl = ReplicateSetTimeline(well=well, replicate_sets=[])
+        for line in data_lines[1:]:
+            split = line.split('\t')
+            time = time_in_seconds(split[0])
+            data = float_or_overflow(split[i])
+            rs = ReplicateSet(time=time, data_points=[data], well=well)
+            rstl.replicate_sets.append(rs)
+        rstls.append(rstl)
+    return rstls
